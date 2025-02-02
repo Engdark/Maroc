200ccdf7a1253198ed6756511682f050fda434e2
Engdark 🌹 <ang089afk@gmail.com>
2025-02-02 03:06:23 +0100

    Update views.py

--- views.py
+++ views.py
@@ -28,7 +28,7 @@ def send_views():
         "Proxy-Connection": "keep-alive",
         "Cache-Control": "max-age=0",
         "Upgrade-Insecure-Requests": "1",
-        "User-Agent": f"Mozilla/5.0 (Linux; Android {random_text_number}; K) AppleWebKit/537.36 (KHTML, like {random_text_number}) Chrome/132.0.0.0 Mobile Safari/{random_text_number}",
+        "User-Agent": f"Mozilla/5.0 ({random_text_number}; Android {random_text_number}; K) AppleWebKit/{random_text_number} (KHTML, like {random_text_number}) Chrome/132.0.0.0 Mobile Safari/{random_text_number}",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
         "Referer": "http://pornhd.josex1.name/popular/3.html",
         "Accept-Encoding": "gzip, deflate",

